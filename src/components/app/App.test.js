9b0dce27883c96dbfaf73a6030f36643bb535b0b
unknown <kandidad@gmail.com>
2022-05-12 13:01:06 +0300

    [TEST] check render App by testid

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-describe('render test', () => {
+describe('App render test', () => {
   let appElement
   beforeEach(() => {
+    render(<App />)
     appElement = null
   })
-  test('renders  App (by text)', () => {
-    render(<App />)
+  test('renders App (by text)', () => {
     appElement = screen.getByText(/react/i)
+    expect(appElement).toBeInTheDocument()
+  })
+  test('renders App (by test id)', () => {
+    appElement = screen.queryByTestId('app-id')
     expect(appElement).toBeInTheDocument()
   })
 })
